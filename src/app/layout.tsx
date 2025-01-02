3179464c69dc638335498b92ee58372af3392ef1
chipanatica <josepaulct@gmail.com>
2025-01-02 16:26:16 +0100

    feat: app title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Navbar } from './components/navbar'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'SpeakMyLanguage',
-  description: 'SpeakMyLanguage is a platform for language learners to practice speaking with native speakers.'
+  title: 'SpeakMentor',
+  description: 'SpeakMentor is a platform for language learners to practice speaking with native speakers.'
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
